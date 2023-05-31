be82352a3129c04374fd3a2105d9f30589257253
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-05-31 18:16:49 -0300

    Conidicionais finalizado

---
+++ 1. Fundamentos/1-periodo/Secao-03/dia-3.1-Introducao-js/exercicio-de-condicionais/conidicionais.js
@@ -0,0 +1,14 @@
+var grade=80;
+let message;
+if (grade >= 80) {
+   message = "Parabéns,você faz parte do grupo de pessoas aprovadas!";
+  console.log(message);
+
+} else if (grade < 80 && grade >= 60) {
+   message = "Você está na nossa lista de espera";
+   console.log(message);
+
+} else if (grade < 60) {
+    message ="Infelizmente,você reprovou";
+    console.log (message);
+}
