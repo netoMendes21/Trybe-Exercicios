e6d9df7eb05755df4c979df67a3441da00e8662f
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-01 15:55:46 -0300

    finalizado o exe3

---
+++ 1. Fundamentos/1-periodo/Secao-03/dia-3.1-Introducao-js/exercicio-3/exercicio3.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 15;
+let num3 = 4;
+
+if ((num1 > num2) && num1 > num3) {
+    console.log(num1);
+
+} else if ((num2 > num1) && num1 > num3) {
+    console.log(num2);
+
+} else if ((num3 > num1) && num3 > num1) {
+    console.log(num3);
+}
