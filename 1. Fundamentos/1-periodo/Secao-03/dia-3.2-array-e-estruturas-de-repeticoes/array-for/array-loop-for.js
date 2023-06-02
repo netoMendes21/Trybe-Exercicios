dfff498b64374ae3fff4cbf477a35a2aee6e20e1
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-02 11:30:50 -0300

    Terceiro passo finalizado

--- 1. Fundamentos/1-periodo/Secao-03/dia-3.2-array-e-estruturas-de-repeticoes/array-for/array-loop-for.js
+++ 1. Fundamentos/1-periodo/Secao-03/dia-3.2-array-e-estruturas-de-repeticoes/array-for/array-loop-for.js
@@ -1,8 +1,11 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaNumbers = 0;
 
+
 for (index = 0; index < numbers.length; index += 1) {
 
-    somaNumbers = somaNumbers + numbers[index];
-}
-console.log(somaNumbers);
+    somaNumbers = (somaNumbers + numbers[index]);
+} 
+
+const media = somaNumbers / numbers.length;
+ console.log (media);
