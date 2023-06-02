2902cddebbea8ba61ca4f82a6cc512cb42d2b4a7
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-02 15:34:05 -0300

    funções com números

---
+++ 1. Fundamentos/1-periodo/Secao-03/dia-3.3-funcoes/funcoes-number.js
@@ -0,0 +1,36 @@
+const balance = 80;
+
+function sumBalance(valor) {
+    return balance + valor;
+};
+
+const resultado = sumBalance(20)
+
+console.log(resultado);
+
+
+function subBalance(valor) {
+    return balance - valor;
+};
+
+const resultado = sumBalance(20)
+
+console.log(resultado);
+
+
+function mulBalance(valor) {
+    return balance * valor;
+};
+
+const resultado = mulBalance(4)
+
+console.log(resultado);
+
+
+function divBalance(valor) {
+    return balance / valor;
+};
+
+const resultado = divBalance(2)
+
+console.log(resultado);
