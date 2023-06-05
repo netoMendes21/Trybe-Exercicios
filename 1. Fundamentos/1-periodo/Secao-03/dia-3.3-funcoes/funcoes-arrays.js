f6cf0c37c3de3180219bcb68ebaac2ad886ed270
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-05 13:57:36 -0300

    quase finalizado

--- 1. Fundamentos/1-periodo/Secao-03/dia-3.3-funcoes/funcoes-arrays.js
+++ 1. Fundamentos/1-periodo/Secao-03/dia-3.3-funcoes/funcoes-arrays.js
@@ -1,6 +1,14 @@
 const trybeBankCustomers = ['Oliva', 'Nat', 'Gus'];
 
+let cliente = "1";
+
 function addCustomer(customer) {
-    
-    
+    if (typeof customer !== "string") {
+        console.log("O parâmetro passado deve ser do tipo string");
+    } else {
+        trybeBankCustomers.push(customer)
+        console.log(trybeBankCustomers);
+    }
 }
+
+addCustomer(cliente);
