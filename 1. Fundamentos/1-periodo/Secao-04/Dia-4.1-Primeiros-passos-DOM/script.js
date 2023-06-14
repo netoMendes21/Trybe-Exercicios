cc499d077074f62f144008f81a9f8ec13775ea9c
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-14 13:45:24 -0300

    requesitos do exercício para fixar feita

--- 1. Fundamentos/1-periodo/Secao-04/Dia-4.1-Primeiros-passos-DOM/script.js
+++ 1. Fundamentos/1-periodo/Secao-04/Dia-4.1-Primeiros-passos-DOM/script.js
@@ -4,10 +4,16 @@ meuTitulo.innerText= 'Jhon Wick';
 const firstParagraph = document.getElementById('first-paragraph');
 firstParagraph.innerText = 'Assassino de aluguel ,que se aposenta e tenta viver uma vida tranquila,porém o seu passado vem ao seu encontro e ele sai da sua vida tranquila para voltar a ser o Baba yaga';
 
-const segundoTitulo = document.getElementsByTagName('h2');
+const segundoTitulo = document.getElementById('subtitle');
 segundoTitulo.innerText = 'Principais informações'
+segundoTitulo.style.color = 'red';
 
-const paragrafoRenomeado = document.getElementsByClassName('paragraph-style');
+const paragrafoRenomeado = document.getElementsByClassName('paragraph-style')[0];
+paragrafoRenomeado.innerHTML = `<i>${paragrafoRenomeado.innerText}</i>`;
 
-  const novoParagrafo = paragrafoRenomeado[0];
-  firstParagraph.style.fontStyle = 'italic';
+// outra opção de colocar o texto em itálico a baixo.
+// paragrafoRenomeado.style.fontStyle = "italic";
+
+const paragrafoInformacoesPrincipais = document.getElementById('second-paragraph');
+
+paragrafoInformacoesPrincipais.innerText = 'Exemplo: Direção: nome da pessoa,roteirista: Nome das pessoas responsáveis pelo roteiro.';
