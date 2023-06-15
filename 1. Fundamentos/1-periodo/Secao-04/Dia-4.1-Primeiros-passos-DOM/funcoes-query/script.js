cc68f7d1e278314986c6679181b39ef08083dd98
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-06-14 21:00:25 -0300

    Depois irei completar o código

--- 1. Fundamentos/1-periodo/Secao-04/Dia-4.1-Primeiros-passos-DOM/funcoes-query/script.js
+++ 1. Fundamentos/1-periodo/Secao-04/Dia-4.1-Primeiros-passos-DOM/funcoes-query/script.js
@@ -1,1 +1,10 @@
 console.log('Hello!');
+
+const cabecalho = document.getElementById('header-container');
+cabecalho.style.backgroundColor = 'rgb(0, 176, 105)';
+
+
+const tarefasUrgentes = document.getElementsByClassName('emergency-tasks')[0];
+tarefasUrgentes.style.backgroundColor = 'rgb(255, 159,132)';
+
+// const 
