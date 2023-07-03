a0afd421e4db4d5c3abf706286cb3e54dac4d40e
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-07-03 16:19:25 -0300

    validando números para Promoção

--- 1. Fundamentos/2-periodo/dia-6.1-fluxos-de-excecao/script.js
+++ 1. Fundamentos/2-periodo/dia-6.1-fluxos-de-excecao/script.js
@@ -24,14 +24,47 @@ button.addEventListener('click', () => {
 const showPromo = (name, number) => {
   const firstText = document.querySelector('#text-initial');
   const secondText = document.querySelector('#text-final');
-  const productObject = checkNumber(parseInt(number));
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    const productObject = checkPromo(number);
+    checkvalidRange(number);
 
-  firstText.innerHTML = `Boas-vindas, ${name}!`;
-  secondText.innerHTML = `A promoção do dia é: 
-    ${productObject.product} no valor de R$ ${productObject.price}`;
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: 
+      ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(err) {
+    secondText.innerHTML = err.message;
+  }
+}
+
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
 }
 
 // Segunda parte
 const checkNumber = (number) => {
-  return promo[number - 1];
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+}
+
+// Terceira parte
+const checkPromo = (number) => {
+  for (let index = 0; index < promo.length; index += 1) {
+    if (number - 1 === index) {
+      return promo[index];
+    }
+  }
+}
+
+const checkvalidRange = (number) => {
+  if (number < 1 || number > 10)  {
+    throw new Error('Insira um número entre 1 e 10')
+    }
 }
