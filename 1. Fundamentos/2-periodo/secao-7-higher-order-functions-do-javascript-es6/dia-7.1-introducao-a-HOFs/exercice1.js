bf613dcd72813c811a254e785422dec8489e8153
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-07-14 15:05:40 -0300

    Parte II feita

---
+++ 1. Fundamentos/2-periodo/secao-7-higher-order-functions-do-javascript-es6/dia-7.1-introducao-a-HOFs/exercice1.js
@@ -0,0 +1,20 @@
+const novaPessoa = (nomeCompleto) => {
+  const pessoa = {
+    nomeCompleto,
+    email: `${nomeCompleto.replace(' ', '_')}@trybe.com`.toLowerCase(),
+  }
+  return pessoa
+};
+// console.log(novaPessoa('Neto'));
+
+
+const newEmployees = (funcao) => {
+  const employees = {
+    id1: funcao('Pedro Guerra'),
+    id2: funcao('Luiza Drumond'),
+    id3: funcao('Carla Paiva'),
+  }
+  return employees;
+};
+
+console.log(newEmployees(novaPessoa));
